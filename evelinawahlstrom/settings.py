cc31eb3b88f4dbbb12428fe4224f613f11280c09
Evelina Wahlström <evelinawahlstrom18@gmail.com>
2019-02-07 17:33:04 +0000

    evelinawahl.pythonanywhere.com

--- evelinawahlstrom/settings.py
+++ evelinawahlstrom/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 ALLOWED_HOSTS = ["127.0.0.1", "pythonanywhere.com"]
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
